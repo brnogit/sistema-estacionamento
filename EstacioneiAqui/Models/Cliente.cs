b639153ec575d47fdc6cd67349715982b135a041
Bruno Mota <bmotadev@gmail.com>
2022-11-22 19:33:39 -0300

    Adiciona a model cliente

---
+++ EstacioneiAqui/Models/Cliente.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EstacioneiAqui.Models
+{
+    public class Cliente
+    {
+        
+    }
+}
