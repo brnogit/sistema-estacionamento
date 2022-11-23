79fb4f3cf05fed762f9acdc25934bcdd90ddbef3
Bruno Mota <bmotadev@gmail.com>
2022-11-23 10:34:46 -0300

    Adiciona propriedades a tabela cliente

--- EstacioneiAqui/Models/Cliente.cs
+++ EstacioneiAqui/Models/Cliente.cs
@@ -7,6 +7,16 @@ namespace EstacioneiAqui.Models
 {
     public class Cliente
     {
-        
+        public int Id { get; set; }
+        public string NomeCliente { get; set; }
+        public string ModeloDoVeiculo { get; set; }
+        public string MarcaDoVeiculo { get; set; }
+        public string Placa { get; set; }
+        public string Categoria { get; set; }
+        public DateTime DataEntrada { get; set; }
+        public DateTime DataSaida { get; set; }
+        public decimal ValorPorHora { get; set; }
+        public decimal ValorTotal { get; set; }
+        public string NomeManobrista { get; set; }
     }
 }
