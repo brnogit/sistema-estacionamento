57824c355732fbc68fcc0812c004854da88cf3a8
Bruno Mota <bmotadev@gmail.com>
2022-11-24 23:08:28 -0300

    Adiciona migrations para tabela clientes

---
+++ EstacioneiAqui/Migrations/20221125020751_CriacaoTabelaCliente.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EstacioneiAqui.Migrations
+{
+    /// <inheritdoc />
+    public partial class CriacaoTabelaCliente : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clientes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NomeCliente = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ModeloDoVeiculo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    MarcaDoVeiculo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Placa = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Categoria = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NumeroDaVaga = table.Column<int>(type: "int", nullable: false),
+                    DataEntrada = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DataSaida = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ValorPorHora = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    ValorTotal = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    NomeManobrista = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clientes", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Clientes");
+        }
+    }
+}
