ec197073c4d3848284000522a4a4d7d30b09bd5c
Bruno Mota <bmotadev@gmail.com>
2022-11-28 20:02:31 -0300

    Adiciona o metodo criar com POST

--- EstacioneiAqui/Controllers/EstacionamentoController.cs
+++ EstacioneiAqui/Controllers/EstacionamentoController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using EstacioneiAqui.Context;
+using EstacioneiAqui.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EstacioneiAqui.Controllers
@@ -26,6 +27,18 @@ namespace EstacioneiAqui.Controllers
         public IActionResult Criar()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Criar(Cliente cliente)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.Clientes.Add(cliente);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(cliente);
         }
     }
 }
